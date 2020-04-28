3e529f73ed2e6e31b301848ff664cc215d7a92ae
annastepanova <annastepanova1811@gmail.com>
2020-04-28 19:41:55 -0400

    built employee table template

---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Harry Potter</td>
+          <td>potter@hogwards.com</td>
+        </tr>
+        <tr>
+          <td>Hermione Granger</td>
+          <td>granger@hogwards.com</td>
+        </tr>
+        <tr>
+          <td>Ron Weasley</td>
+          <td>weasley@hogwards.com</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'employee-table',
+  }
+</script>
+
+<style scoped></style>
